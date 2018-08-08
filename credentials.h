c3d9a2760d2434a48246e4b5a61fbe2b5337b036
Carl <fonts@spoturdeal.com>
2018-08-08 08:55:05 +0200

    File added for credentials outside main code

---
+++ credentials.h
@@ -0,0 +1,26 @@
+
+
+// here are the credentials you need in your sketch
+
+//  ip_MQTT could be an IP address or a server name like broker.example.com
+//  if you use a different port for your MQTT set the proper port
+//  OTA makes it possible to update over the air without pressing a button
+
+
+//                  WIFI
+
+#define my_SSID "***YOUR_WIFI_SSID"      // ssid of your accesspoint
+#define my_PASSWORD "***YOUR_WIFI_PASSWORD"  // password of access point
+
+//                  MQTT
+
+#define ip_MQTT "192.168.xxx.xxx"
+#define port_MQTT 1883
+#define my_MQTT_USER "***YOUR_MQTT_USERNAME"       // user for the mqtt server
+#define my_MQTT_PW "**YOUR_MQTT_PASSWORD"  // password of mqtt
+
+//                   OTA
+
+#define my_OTA_PW "***YOUR_OTA_PASSWORD"  // password for OTA updates
+
+
